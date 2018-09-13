c3081ab1986d5a63af5e08c37c563d3be7a4b3a3
Sara Harvey <saraharveynyc@gmail.com>
2018-09-13 19:59:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli = []
-  puts "There is nobody waiting to be served."
+  puts "There is nobody waiting to be served!"
   else 
   puts "Welcome, #{name[0]}."
 end
